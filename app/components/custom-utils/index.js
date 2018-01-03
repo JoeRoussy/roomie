25a799d3b8392412382b2f79140f80d8d00b8cdd
Joseph Roussy <joeroussy@gmail.com>
2018-01-02 22:34:37 -0500

    Added a database configuration

---
+++ app/components/custom-utils/index.js
@@ -0,0 +1,95 @@
+import { ObjectId } from 'mongodb';
+
+export const required = (param, customMessage) => {
+    if (typeof customMessage === 'string') {
+        throw new Error(`could not find required param: ${param}. ${customMessage}`);
+    } else {
+        throw new Error(`${param} is required`);
+    }
+}
+
+export const print = (obj, message) => {
+    if (message) {
+        console.log(message);
+    }
+
+    console.log(JSON.stringify(obj, null, 4));
+}
+
+export const stringify = obj => JSON.stringify(obj, null, 4);
+
+// Takes a list and intersects and intersects it with another list. However, if the
+// first list is empty, we return the second list.
+// Can pass in a comparator for array of objects. The comparator will be called with the source
+// array and an element from the target array and should return if the element is in the source array
+export const intersectIfPopulated = (target, source, comparator) => {
+    if (!target.length) {
+        return source;
+    }
+
+    return target.filter(x => {
+        if (comparator && typeof comparator === 'function') {
+            return comparator(x, source);
+        }
+
+        return source.indexOf(x) !== -1;
+    });
+}
+
+export const convertToObjectId = id => {
+    if (typeof id === 'number' || typeof id === 'string') {
+        return ObjectId(id);
+    }
+
+    return id;
+}
+
+// Returns a function to sort an array of objects by key
+// Objects in the key must be strings or numbers (comparable using '<' and '>')
+export const sortByKey = key => (a, b) => {
+    if (a[key] > b[key]) {
+        return 1;
+    }
+
+    if (a[key] < b[key]) {
+        return -1;
+    }
+
+    return 0;
+}
+
+// Uses the createdAt property in each object
+export const sortByDate = (a, b) => b.createdAt - a.createdAt;
+
+// Returns a fully qualified URL with a given path using the slug in a req object
+export const buildUrl = (req, path) => `${req.protocol}://${req.get('host')}${path}`;
+
+// Takes an array and returns all the unique values
+export const unique = array => {
+    let result = [];
+
+    for (let i = 0; i < array.length; i++) {
+        if(!result.includes(array[i])) {
+            result.push(array[i]);
+        }
+    }
+
+    return result;
+};
+
+// Retuns a regex for a plain language query that may contain multiple words using and logic
+// NOTE: For compatability with mongodb, this function returns a string and not a RegExp object
+export const getRegex = query => {
+    let regex = '';
+    const words = query.split(' ');
+
+    if (words.length === 1) {
+        // If there is only 1 word in the name query, use that
+        regex = words[0];
+    } else {
+        // If there are multiple words in the name query, use and logic in the regex
+        regex = words.reduce((r, word) => `${r}(?=.*${word})`, '');
+    }
+
+    return regex;
+}
--- app/server.js
+++ app/server.js
@@ -1,12 +1,45 @@
 import express from 'express';
 
+import { getLogger, getChildLogger } from './components/log-factory';
+import dbConfig from './components/db/config';
 
 const app = express();
 
-// TODO(Joe): DB setup(including driver), logging setup, app and api routes
-
-app.get('/', (req, res) => {
-    res.send('Hello world!');
+global.Logger = getLogger({
+    name: 'roomie'
 });
 
-app.listen(3000, () => console.log('App listening on port 3000'));
+const dbLogger = getChildLogger({
+    baseLogger: Logger,
+    additionalFields: {
+        module: 'db-config'
+    }
+});
+
+// TODO(Joe): DB setup(including driver), logging setup, app and api routes
+
+// Set app in an IIFE so we can bail using return if things so not initialize properly
+(() => {
+    let db = null;
+
+    try {
+        db = dbConfig();
+    } catch (e) {
+        dbLogger.error(e, 'Error connecting to database');
+
+        return;
+    }
+
+    if (!db) {
+        dbLogger.error(null, 'Got null or undefinded for the db connection from the db config');
+
+        return;
+    }
+
+    // TODO: Configure actual routes here:
+    app.get('/', (req, res) => {
+        res.send('Hello world!');
+    });
+
+    app.listen(3000, () => Logger.info('App listening on port 3000'));
+})();

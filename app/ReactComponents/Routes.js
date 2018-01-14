20cec375e3beb741637aa8df31f93d8dee9631cc
Brandon Baksh <brandon.baksh@hotmail.com>
2018-01-14 16:39:06 -0500

    Added redux integration

--- app/ReactComponents/Routes.js
+++ app/ReactComponents/Routes.js
@@ -1,13 +1,46 @@
 import React from 'react';
 import { Button } from 'semantic-ui-react';
+import { connect } from 'react-redux'; 
+import { makeUppercase, makeLowercase } from '../redux/actions/sampleActions';
+
+@connect((store)=>{
+	return {
+		projectName: store.sampleReducer.name,
+		owners: store.sampleReducer.owners
+	}
+}) 
 class Routes extends React.Component {
+	constructor(){
+		super();
+
+		this.clickNormalButton = this.clickNormalButton.bind(this);
+		this.clickSemanticButton = this.clickSemanticButton.bind(this);
+		this.getProjectName = this.getProjectName.bind(this);
+		this.getOwners = this.getOwners.bind(this);
+	}
+	clickNormalButton(){
+		this.props.dispatch(makeLowercase());
+	}
+	clickSemanticButton(){
+		this.props.dispatch(makeUppercase());
+	}
+	getProjectName(){
+		return this.props.projectName;
+	}
+	getOwners(){
+		return this.props.owners;
+	}
 	/* Create rendering routes here? */
 	render(){
+		const names = this.getOwners().map((owner)=>{
+			return <li> {owner} </li>
+		});
 		return (
 			<div>
 				Hello World!
-				<button>Normal Button</button>
-				<Button>Semantic Button</Button>
+				<ul> {names} </ul>
+				<button onClick={this.clickNormalButton}>Normal Button</button>
+				<Button onClick={this.clickSemanticButton}>Semantic Button</Button>
 			</div>
 		)
 	}
--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,8 @@ module.exports = {
 				exclude: /node_modules/,
 				loader: 'babel-loader',
 				query: {
-					presets: ['react', 'es2015']
+					presets: ['react', 'es2015', 'stage-0'],
+					plugins: ['transform-class-properties', 'transform-decorators-legacy']
 				}
 			},
 			{

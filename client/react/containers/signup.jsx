21859d2aebc50bcc13c3ec167bb7b6df5ddc5542
Joseph Roussy <joeroussy@gmail.com>
2018-01-22 11:26:14 -0500

    Added redux forms

--- client/react/containers/signup.jsx
+++ client/react/containers/signup.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import SignUpForm from '../components/signupForm';
+
+const onSubmit = (e) => {
+
+};
 
 const signup = () => (
     <div>
         <h1>Sign up</h1>
+        <SignUpForm onSubmit={onSubmit}></SignUpForm>
     </div>
 );
 
--- client/redux/reducers/root.js
+++ client/redux/reducers/root.js
@@ -4,11 +4,13 @@ import { combineReducers } from 'redux';
 import sampleReducer from './sampleReducer';
 import listingsReducer from './listingsReducer';
 import userReducer from './userReducer';
+import { reducer as formReducer } from 'redux-form';
 
 const reducers = combineReducers({
 	sampleReducer,
     listingsReducer,
-    userReducer
+    userReducer,
+    form: formReducer
 });
 
 export default reducers;

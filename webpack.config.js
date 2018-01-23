cec330f04929b16244a286af637fc72418b55893
Alex Lee Chan <firefly1585@gmail.com>
2018-01-23 10:37:47 -0500

    Added Css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,39 +3,44 @@ var webpack = require('webpack');
 var Dotenv = require('dotenv-webpack');
 
 module.exports = {
-	entry: ['./client/client.js'],
-	output: {
-		filename: 'bundle.js',
-		path: path.join(__dirname, 'public')
-	},
-	module: {
-		loaders: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				loader: 'babel-loader',
-				query: {
-					presets: ['react', 'es2015', 'stage-0']
-				}
-			},
-			{
-				test: /\.jsx$/,
-				exclude: /node_modules/,
-				loader: 'babel-loader',
-				query: {
-					presets: ['react', 'es2015', 'stage-0'],
-					plugins: ['transform-class-properties', 'transform-decorators-legacy']
-				}
-			},
-			{
-				test: /\json$/,
-				loader: 'json-loader'
-			}
-    	]
-	},
-	resolve: {
-    	extensions: ['.js', '.jsx', '.json']
-  	},
+    entry: ['./client/client.js'],
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'public')
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015', 'stage-0']
+                }
+            },
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015', 'stage-0'],
+                    plugins: ['transform-class-properties', 'transform-decorators-legacy']
+                }
+            },
+            {
+                test: /\json$/,
+                loader: 'json-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader' ]
+            }
+        ],
+
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.json']
+      },
     plugins: [
         new Dotenv({
             path: './.env'

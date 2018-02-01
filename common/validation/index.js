93ac35cc251234b4cd0be12c0c804bdc6af40a4a
Joseph Roussy <joeroussy@gmail.com>
2018-02-01 14:22:45 -0500

    Added email and password validation to the sign in component

---
+++ common/validation/index.js
@@ -0,0 +1,38 @@
+export const isEmpty = (value) => !value;
+
+export const isText = (value) => {
+    // We accept strings and numbers as text
+    if (!(typeof value === 'string' || typeof value === 'number')) {
+        return false;
+    }
+
+    return !!value;
+}
+
+export const email = (value) => {
+    if (!isText(value)) {
+        return false;
+    }
+
+    const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    return regex.test(value);
+}
+
+export const password = (value, checkLength) => {
+    if (!isText(value)) {
+        return false;
+    }
+
+    if (!checkLength) {
+        // We have text and we don't need to check the length so return true
+        return true;
+    }
+
+    const {
+        MIN_PASSWORD_LENGTH
+    } = process.env;
+
+    // Coerce password length into a number before making the comparisson
+    return value.length >= +MIN_PASSWORD_LENGTH;
+}

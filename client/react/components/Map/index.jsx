474e9e225447582133ff76e5fea75a4d5ed324c3
Brandon Baksh <brandon.baksh@hotmail.com>
2018-03-20 20:28:22 -0400

    Demoable map feature - TODO: get it to recenter on new listings

--- client/react/components/Map/index.jsx
+++ client/react/components/Map/index.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import {Map, Marker, InfoWindow, GoogleApiWrapper} from 'google-maps-react';
+import { Dimmer, Loader } from 'semantic-ui-react';
+
 
 class MapComponent extends Component {
     constructor(props){
@@ -8,36 +11,54 @@ class MapComponent extends Component {
     }
 
     fetchPlaces() {
-        this.props.google.initialCenter = this.props.position;
+        const {
+            google
+        } = this.props;
+        const maps = google.maps;
+        const mapRef = this.refs.map;
+        const node = ReactDOM.findDOMNode(mapRef);
+        const center = new maps.LatLng(this.props.position.lat, this.props.position.lng);
+        const mapConfig = Object.assign({},{
+            center: center,
+            zoom: 15
+        })
+        this.map = new maps.Map(node, mapConfig);
     }
+
 
     render(){
         const style = {
             width: '60%',
             height: '60%'
         }
-
         const zoom = 15;
-
         const {
             position
         } = this.props
-        console.log(position)
 
         const marker = <Marker 
             position = {position}
         />
 
-         return (
-            <Map    
+        if(!this.props.loaded){
+            return (
+                <Dimmer active>
+                    <Loader>Loading Map...</Loader>
+                </Dimmer>
+            )
+        }
+
+        return (
+            <div>
+                <Map    
                     google = {this.props.google}
                     style = {style}
                     zoom = {zoom}
                     initialCenter={position}
-                    onReady = {this.fetchPlaces}
-            >
-                {marker}
-            </Map>
+                >
+                    {marker}
+                </Map>
+            </div>
         )
     }
 }
